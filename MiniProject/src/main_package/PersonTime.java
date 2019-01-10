57e391d59a5b57e28b1f648d6b4c3d76b72df1a7
unknown <shab.pompeiano@gmail.com>
2019-01-10 18:00:12 +0100

    Adding files back

---
+++ MiniProject/src/main_package/PersonTime.java
@@ -0,0 +1,19 @@
+package main_package;
+
+
+public class PersonTime {
+	private String ID; //Id of the person
+	private int time; //time in minutes
+	
+	public String getID() {
+		return this.ID;
+	}
+	
+	public int getTime() {
+		return this.time;
+	}
+	
+	public void setTime(int time) {
+		this.time = time;
+	}
+}
