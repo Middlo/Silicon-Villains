69324bc269f371a60b2d392c78625ba99a7d595e
7SJ7 <stankojankov99@gmail.com>
2019-01-13 03:09:46 +0100

    change project

--- MiniProject/src/main_package/Project.java
+++ MiniProject/src/main_package/Project.java
@@ -1,6 +1,5 @@
 package main_package;
 
-
 import java.util.ArrayList;
 
 public class Project {
@@ -8,7 +7,7 @@ public class Project {
 	private String ID;
 	private String name;
 	private String desc;
-	private int duration;//in weeks
+	private int duration;// in weeks
 	private int budget;
 	private int RoI;
 	private ArrayList<PersonTime> times; // should be array with each member and their time
@@ -24,8 +23,9 @@ public class Project {
 		this.times = new ArrayList<>();
 		this.tasks = new ArrayList<>();
 	}
-	
-	public Project(String ID, String name, String desc, int duration, int budget, int RoI, ArrayList<PersonTime> times, ArrayList<Task> tasks) {
+
+	public Project(String ID, String name, String desc, int duration, int budget, int RoI, ArrayList<PersonTime> times,
+			ArrayList<Task> tasks) {
 		this.ID = ID;
 		this.name = name;
 		this.desc = desc;
@@ -43,121 +43,143 @@ public class Project {
 		cost_Variance = earned_Value - this.budget;
 		return cost_Variance;
 	}
-	
+
 	public double scheduleVariance(int current_week) {
-		
-		return (this.earnedValue(current_week)-this.plannedValue(current_week));
 
+		return (this.earnedValue(current_week) - this.plannedValue(current_week));
 
 	}
+
 	public double plannedValue(int current_week) {
-		
-		int projectDuration=this.duration;
-		
-		double percentageOfProjectDone=(double)(current_week/projectDuration);
-		double plannedValue=percentageOfProjectDone*this.budget;
-		
+
+		int projectDuration = this.duration;
+
+		double percentageOfProjectDone = (double) (current_week / projectDuration);
+		double plannedValue = percentageOfProjectDone * this.budget;
+
 		return plannedValue;
-		
+
 	}
+
 	public ArrayList<PersonTime> timePerMember() {
 		return this.times;
 	}
 
 	public boolean updateTime(String personID, int time) {
 		boolean found = false;
-		for(PersonTime couple : this.times) {
-			if(personID.equals(couple.getID())) {
+		for (PersonTime couple : this.times) {
+			if (personID.equals(couple.getID())) {
 				couple.setTime(time);
 				found = true;
 			}
 		}
 		return found;
 	}
-	
-	public void createTask(String name, int start_week,int end_week,int sn) {
+
+	public void createTask(String name, int start_week, int end_week, int sn) {
 		this.tasks = new ArrayList<>();
-		Task task = new Task(name, start_week, end_week,sn);
+		Task task = new Task(name, start_week, end_week, sn);
 		this.tasks.add(task);
 	}
+
 	public int getNumberOfTasks() {
 		return tasks.size();
 	}
+
 	public Task getTask(int position) {
 		return this.tasks.get(position);
 	}
+
 	public double retrieveCompletedPercentage(int week) {
-		
-		double completedPercentage=0.0;
-		int completedTasks=0;
-		int allTasks=0;
+
+		double completedPercentage = 0.0;
+		int completedTasks = 0;
+		int allTasks = 0;
 
 		for (Task task : this.tasks) {
-			if(tasks != null) {
-				allTasks+=task.LengthOfTask();
-				if(task.completedTask(week))
-					completedTasks+=task.LengthOfTask();
+			if (tasks != null) {
+				allTasks += task.LengthOfTask();
+				if (task.completedTask(week))
+					completedTasks += task.LengthOfTask();
 			}
 		}
-		completedPercentage=allTasks/completedTasks;
+		completedPercentage = allTasks / completedTasks;
 		return completedPercentage;
 	}
+
 	public double earnedValue(int current_week) {
-		
-		double earned_value=0;
-		double completed_work=retrieveCompletedPercentage(current_week);
-		
-		earned_value=this.budget*completed_work;
+
+		double earned_value = 0;
+		double completed_work = retrieveCompletedPercentage(current_week);
+
+		earned_value = this.budget * completed_work;
 		return earned_value;
 
 	}
-	
-	public int[] scheduleActivity(int position) {
-		int startWeek = this.tasks.get(position).getStart_week();
-		int endWeek = this.tasks.get(position).getEnd_week();
-		
-		return new int[] {startWeek, endWeek};
+
+	public String scheduleActivity() {
+
+		final String END_OF_LINE = System.lineSeparator();
+		String projectSchedule = "Project Schedule" + END_OF_LINE;
+		projectSchedule += END_OF_LINE;
+		projectSchedule += "Task Name   " + "Start Week   " + "End Week   " + END_OF_LINE;
+		for (Task task : this.tasks) {
+			if (tasks != null) {
+				projectSchedule += task.getTaskName() + "   " + task.getStart_week() + "   " + task.getEnd_week()
+						+ END_OF_LINE;
+			}
+		}
 	}
-	
-	public String riskMatrix(int probability, int impact) {
-		
+
+	public String riskMatrix(String risk_name, int probability, int impact) {
+
 		int riskMatrix = probability * impact;
+		String risk=null;
 		
 		if (riskMatrix > 3) {
 			if (riskMatrix > 7) {
 				if (riskMatrix > 11) {
-					return "Dangerous";
+					risk="Dangerous";
 				}
-				return "High";
+				risk="High";
 			}
-			return "Medium";
-		}	
-		return "Safe";
+			risk="Medium";
+		}
+		risk="Safe";
+		
+		final String END_OF_LINE = System.lineSeparator();
+		String rMatrix = "Risk Matrix" + END_OF_LINE;
+		rMatrix += END_OF_LINE;
+		rMatrix += "Risk Name   " +"Probability   "+"Impact   "+"Risk"+ END_OF_LINE;
+		rMatrix += risk_name+"   " +probability+"   "+impact+"   "+risk+ END_OF_LINE;
+		
+		return rMatrix;
 	}
-	
+
 	public void addTime(String ID, int time) {
-		PersonTime couple = new PersonTime(ID,time);
+		PersonTime couple = new PersonTime(ID, time);
 		this.times.add(couple);
-		System.out.println("Added "+couple+" To the array of TIMES");
+		System.out.println("Added " + couple + " To the array of TIMES");
 	}
-	
+
 	public void removeTime(String ID) {
-		//Function called to remove the element in the ArrayList times (attribute of a project)
-		//that has as ID of the member the given "ID"
+		// Function called to remove the element in the ArrayList times (attribute of a
+		// project)
+		// that has as ID of the member the given "ID"
 		PersonTime toRemove = null;
-		for(PersonTime each : this.times) {
-			if(each.getID().equals(ID)) {
+		for (PersonTime each : this.times) {
+			if (each.getID().equals(ID)) {
 				toRemove = each;
 			}
 		}
 		times.remove(toRemove);
 	}
-	
+
 	public boolean hasMember(String ID) {
-		//Function that returns true if the member with id "ID" is present
-		//in the ArrayList times (attribute of a project)
-		for(PersonTime each : this.times) {
-			if(each.getID().equals(ID)) {
+		// Function that returns true if the member with id "ID" is present
+		// in the ArrayList times (attribute of a project)
+		for (PersonTime each : this.times) {
+			if (each.getID().equals(ID)) {
 				return true;
 			}
 		}
@@ -188,11 +210,11 @@ public class Project {
 		return this.RoI;
 	}
 
-	public ArrayList<PersonTime> getTimes(){
+	public ArrayList<PersonTime> getTimes() {
 		return this.times;
 	}
-	
-	public ArrayList<Task> getTasks(){
+
+	public ArrayList<Task> getTasks() {
 		return this.tasks;
 	}
 
@@ -215,7 +237,7 @@ public class Project {
 	public void setRoI(int roI) {
 		RoI = roI;
 	}
-	
+
 	public String toString() {
 
 		final String END_OF_LINE = System.lineSeparator();
@@ -226,16 +248,15 @@ public class Project {
 		projectInfo += "Budget: " + this.budget + " SEK per month" + END_OF_LINE;
 		projectInfo += END_OF_LINE + "********* Extra Info *********";
 		projectInfo += END_OF_LINE + "Group members:" + END_OF_LINE;
-		if(this.times.size()==0) {
+		if (this.times.size() == 0) {
 			projectInfo += "None" + END_OF_LINE;
-		}else {
-			for(PersonTime each : this.times) {
+		} else {
+			for (PersonTime each : this.times) {
 				projectInfo += "Member " + each.getID() + " spent " + each.getTime() + " minutes" + END_OF_LINE;
 			}
 		}
-		
+
 		return projectInfo;
 	}
 
 }
-
